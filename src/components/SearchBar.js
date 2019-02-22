2ead0a6f78ee2374b6fdbd85181f291a02dfaf22
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-21 20:50:35 -0500

    Made SearchBar into controlled component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
   render() {
     return (
       <div className='ui large icon input search-bar'>
         <input
+          onChange={this.onInputChange}
+          value={this.state.term}
           type='text'
           className='centerInput fluid'
           placeholder='Enter Km...'
