3356b119fd56f288995286dd3e0c1a11f3435b21
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-21 20:47:46 -0500

    Finished styling for SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className='app ui container'>
-        <h1 className='ui header centered '>
+        <h1 className='ui header centered'>
           Get Outside
           <div className='sub header'>Exercise. Explore. Get outside.</div>
         </h1>
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,17 @@
-import
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className='ui large icon input search-bar'>
+        <input
+          type='text'
+          className='centerInput fluid'
+          placeholder='Enter Km...'
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
