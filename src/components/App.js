ce0c8f64b037c276954caf01e9e335022cb6987b
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-21 19:48:35 -0500

    Added some styling to header and subheader

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className='app ui container'>
+        <h1 className='ui header centered '>
+          Get Outside
+          <div className='sub header'>Exercise. Explore. Get outside.</div>
+        </h1>
+      </div>
+    );
   }
 }
 
