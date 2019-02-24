7db4656ccab2f598eee87154991dfed8c51606e0
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-24 11:22:57 -0500

    Updates

---
+++ precache-manifest.34b79f5876b7faf071fb1ed0dc51fa40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "751a87bd08262143bca7",
+    "url": "/Bits-Into-Bites/static/js/runtime~main.751a87bd.js"
+  },
+  {
+    "revision": "4596a5b16a9d50898f9f",
+    "url": "/Bits-Into-Bites/static/js/main.4596a5b1.chunk.js"
+  },
+  {
+    "revision": "e23185d3994407b77919",
+    "url": "/Bits-Into-Bites/static/js/2.e23185d3.chunk.js"
+  },
+  {
+    "revision": "4596a5b16a9d50898f9f",
+    "url": "/Bits-Into-Bites/static/css/main.0fec3d3c.chunk.css"
+  },
+  {
+    "revision": "f96a80cf9c0d64462e0acade72615ae1",
+    "url": "/Bits-Into-Bites/index.html"
+  }
+];
