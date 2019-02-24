cac409c9466158c93a7eb2360a5c08087f61b6f0
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-24 11:04:41 -0500

    Set up distinct colors for each health label

--- src/components/HealthLabel.js
+++ src/components/HealthLabel.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 const HealthLabel = ({ label }) => {
-  if (label) {
-    var labelArray = label.split('-');
+  var labelArray = label.split('-');
 
-    var newLabel = '';
-    for (let i = 0; i < labelArray.length; i++) {
-      newLabel = newLabel + labelArray[i];
-    }
+  var newLabel = '';
+  for (let i = 0; i < labelArray.length; i++) {
+    newLabel = newLabel + labelArray[i];
   }
 
-  const healthColors = {
-    Vegan: '',
-    Vegetarian: '',
-    PeanutFree: '',
-    TreeNutFree: '',
-    AlcoholFree: ''
+  const colors = {
+    Vegan: '#3D9970',
+    Vegetarian: '#2ECC40',
+    PeanutFree: '#FFDC00',
+    TreeNutFree: '#FF851B',
+    AlcoholFree: '#B10DC9',
+    SugarConscious: '#7FDBFF',
+    AlcoholCocktail: '#85144b'
   };
 
-  return <div className='ui label'>{label}</div>;
+  const background = colors[newLabel];
+
+  return (
+    <div className='ui label' style={{ backgroundColor: background }}>
+      {label}
+    </div>
+  );
 };
 
 export default HealthLabel;
